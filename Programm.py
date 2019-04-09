dc617869ceb851aace34c25a8e904f4499247217
Nießing <matthias.niessing@fh-muenster.de>
2019-04-09 11:23:34 +0200

    neuer Code
    Codecode

--- Programm.py
+++ Programm.py
@@ -1,5 +1,14 @@
-def f1():
-    for i in range(0,11):
-        print(i,end="")
 
-f1()
+def f1(zahl):
+    L=[]
+    z=zahl
+    for i in range(0,8):
+        L.append(zahl%2)
+        zahl = zahl >>1
+    L.reverse()
+    print("Die Zahl %d binär: "%(z),L)
+
+zahl = 5
+f1(zahl)
+f1(~zahl)
+f1(~zahl+1)
